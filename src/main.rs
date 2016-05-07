4970bc1ee2b8ffd2650c1452a25e5ba9588e6490
Nico <nico@Nicos-MacBook-Pro.local>
2016-05-06 20:27:36 -0400

    lots o work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use std::fs::File;
+
 mod nsf;
+mod instructions;
+mod nesmemory;
+mod machine;
 
 fn main() {
-    println!("hello world");
-    println!("{}", std::mem::size_of::<nsf::NsfHeader>());
+    println!("{}", std::mem::size_of::<instructions::Full6502::Instruction>());
+
+    let f = File::open("../testfiles/simple.nsf");
+    match f {
+        Err(_) => {println!("Failed to open file.")},
+        Ok(f) => {
+            let header = nsf::read_nsf_header(f);
+            println!("{:?}", header);
+            // f is closed when it goes out of scope here  
+        }
+    }
+
 }
--- src/nsf.rs
+++ src/nsf.rs
@@ -1,5 +1,6 @@
 use std::io::*;
 
+#[derive(Debug)]
 pub struct NsfHeader {
 	num_songs : u8,
 	start_song : u8,
@@ -14,18 +15,23 @@ pub struct NsfHeader {
 	standard : VideoStandard,
 }
 
+#[derive(Debug)]
 pub enum VideoStandard {
 	NTSC, PAL, Both
 }
 
-fn read_nsf_header<T : Read> (mut reader : T)-> Result<NsfHeader> {
+pub fn read_nsf_header<T : Read> (mut reader : T)-> Result<NsfHeader> {
 
+	// read into a buffer of 128 bytes; if that didn't work, error out
 	let mut buff : [u8; 128] = [0; 128];
 	match reader.read(&mut buff) {
 		Err(e) => return Err(e),
 		Ok(n) if n < 128 => return Err(Error::new(ErrorKind::UnexpectedEof, "Expected at least 128 bytes for NSF header.")),
 		_ => {} 
 	}
+
+	// function to read a word (2 bytes) from the buffer
+	let word = |i : usize| buff[i] as u16 + (buff[i+1] as u16 * 256);
 
 	// first five bytes must be: 4e 45 53 4d 1a
 	assert!(buff[0] == 0x4e);
@@ -34,14 +40,24 @@ fn read_nsf_header<T : Read> (mut reader : T)-> Result<NsfHeader> {
 	assert!(buff[3] == 0x4d);
 	assert!(buff[4] == 0x1a);
 
+	// version number must be 1
+	assert!(buff[5] == 1);
+
+	let standard_byte = match buff[0x7A]{
+		0 => VideoStandard::NTSC,
+		1 => VideoStandard::PAL,
+		2 | 3 => VideoStandard::Both,
+		_ => return Err(Error::new(ErrorKind::UnexpectedEof, "PAL/NTSC byte was malformed."))
+	};
+
 	Ok(NsfHeader{
-		num_songs : 0,
-		start_song : 0,
-		load_address : 0,
-		init_address : 0,
-		play_address : 0,
-		play_speed_pal : 0,
-		play_speed_ntsc : 0,
-		standard : VideoStandard::NTSC,
+		num_songs : buff[0x6],
+		start_song : buff[0x7],
+		load_address : word(0x8),
+		init_address : word(0xA),
+		play_address : word(0xC),
+		play_speed_ntsc : word(0x6E),
+		play_speed_pal : word(0x78),
+		standard : standard_byte,
 	})
 }
